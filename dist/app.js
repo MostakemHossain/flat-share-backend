572b40a86a6a5fd4fa45b20a8f29ec5c60b9a36d
MostakemHossain <mostakememon123@gmail.com>
2024-10-20 23:10:13 +0600

    credentials include

--- dist/app.js
+++ dist/app.js
@@ -11,35 +11,10 @@ const notFound_1 = __importDefault(require("./app/middlewares/notFound"));
 const routes_1 = __importDefault(require("./app/routes"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-const allowedOrigins = [
-    "https://flat-match-frontend.vercel.app/",
-    // "http://localhost:5173",
-];
-// parsers
-app.use(express_1.default.json());
-// CORS configuration
 app.use((0, cors_1.default)({
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        }
-        else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
+    origin: "*",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
 }));
-app.use((req, res, next) => {
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.header("Access-Control-Allow-Origin", origin);
-    }
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
-    next();
-});
 app.use((0, cookie_parser_1.default)());
 app.use("/api/v1", routes_1.default);
 app.get("/", (req, res) => {
--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,30 @@ import express, { Application } from "express";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
 import router from "./app/routes";
+
 const app: Application = express();
 
 app.use(express.json());
+
+const allowedOrigins = [
+  "https://flat-match-frontend.vercel.app",
+  "http://localhost:3000",
+];
+
+// CORS configuration
 app.use(
   cors({
-    origin: "*", 
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
+
 app.use(cookieParser());
 
 app.use("/api/v1", router);
@@ -23,10 +38,10 @@ app.get("/", (req, res) => {
   });
 });
 
-// global error handler
+// Global error handler
 app.use(globalErrorHandler);
 
-// not found Routes
+// Not found routes
 app.use(notFound);
 
 export default app;
