0cc3c17a2481e9f3b3205f65ba94cf255091c7b6
MostakemHossain <mostakememon123@gmail.com>
2024-10-20 22:57:39 +0600

    modification

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,17 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://flat-match-frontend.vercel.app"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(cookieParser());
 
 app.use("/api/v1", router);
